42f8ce04fe7f4f1b40b39cb122550f809578c22a
Nick <36767444+njcos@users.noreply.github.com>
2025-02-07 17:06:06 -0500

    Functionality works

--- script.js
+++ script.js
@@ -1,41 +1,67 @@
 const gameBoard = (function() {
-    const gameArray = [1,2,3,4,5,6,7,8,9]
+    const gameArray = [1,2,3,4,5,4,7,8,9]
 
-    
     function pick(index, mark) {
         gameArray.splice(index, 1, mark)
-        console.log(gameArray)
     }
+
     const board = () => gameArray;
     return {pick, board}
 })();
 
 function player(name) {
     const userName = name;
-    const userMark = "X";
+    const userMark = "x";
     console.log(`Username: ${userName}, Mark: ${userMark}`)
     return {userName, userMark}
 }
 
-function game() {
-    // let pick = prompt("pick your spot:");
-    // gameBoard.pick(pick, "x")
+function game() {  
+    pick()
 
-    const comPick = () => {
-        let pick = Math.floor(Math.random()*9)
-
-
-        if(typeof gameBoard.board()[pick] === 'number'){
-            gameBoard.pick(pick, "O")
-            console.log(gameBoard.board())
-            comPick()
-        } else {
-            console.log('Seats taken')
-            
+    function lines (mark) {
+        const markIndex =[];
+        let xLocation = gameBoard.board().indexOf(mark)
+        while (xLocation !== -1) {
+            markIndex.push(xLocation);
+            xLocation = gameBoard.board().indexOf(mark, xLocation + 1)
         }
+        return markIndex.toString()
+    }
+    
+    function pick() {      
+        if(lines("x") === "0,1,2" || lines("x") === "3,4,5" || lines("x") === "6,7,8" || lines("x") === "0,4,8" || lines("x") === "2,4,6"){
+            console.log('You win!')
+        } else if(lines("o") === "0,1,2" || lines("o") === "3,4,5" || lines("o") === "6,7,8" || lines("o") === "0,4,8" || lines("o") === "2,4,6")  {
+            console.log('Computer wins!')
+        } else if (gameBoard.board().every(isNaN)){
+            console.log("it's a tie")
+        } else {
+            const pick = prompt("pick your spot:");
+            gameBoard.pick(pick, "x")
+            comPick()
+        }
+
     }
 
-    comPick()
+    function comPick(){
+        if(lines("x") === "0,1,2" || lines("x") === "3,4,5" || lines("x") === "6,7,8" || lines("x") === "0,4,8" || lines("x") === "2,4,6"){
+            console.log('You win!')
+        } else if(lines("o") === "0,1,2" || lines("o") === "3,4,5" || lines("o") === "6,7,8" || lines("o") === "0,4,8" || lines("o") === "2,4,6")  {
+            console.log('Computer wins!')
+        } else if (gameBoard.board().every(isNaN)){
+            console.log("it's a tie")
+        } else {
+            let cPick = Math.floor(Math.random()*9);
+            if(typeof gameBoard.board()[cPick] === 'number'){
+                gameBoard.pick(cPick, "O")
+                console.log(gameBoard.board())
+                pick()
+            } else {
+                comPick()
+            }
+        }
+    }
 }
 
 game()
