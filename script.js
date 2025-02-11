dd3853940b9e4773acc5d511addc8a70d445f4d8
Nick <36767444+njcos@users.noreply.github.com>
2025-02-10 21:52:27 -0500

    Functionality complete
    Completed the functionality with input validation.  Everthing is set for UI.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const gameBoard = (function() {
     const gameArray = [1,2,3,4,5,4,7,8,9]
 
@@ -23,10 +24,7 @@ function game() {
         return markIndex.toString()
     }
 
-    function cPick() {
-        const computerPick = Math.floor(Math.random()*9)
-        return computerPick
-    }
+
 
     function players () {
         playerArray = [
@@ -44,30 +42,60 @@ function game() {
         return playerArray
     }
 
-    pick(players())
+
+    let currentPlayer = 1;
+
+    function activePlayer () {
+        if(currentPlayer === 0){
+            return currentPlayer = 1;
+        } else {
+            return currentPlayer =0;
+        }
+    }
+
+
+    pick(players()[activePlayer()])
     function pick(player) {    
-        let activePlayer = player[0]
-        let message = ""
+        let currentPlayer = player;
+        let message = "";
+        console.log(lines('x'))
+        console.log(lines('o'))
+
         if(lines("x") === "0,1,2" || lines("x") === "3,4,5" || lines("x") === "6,7,8" || lines("x") === "0,4,8" || lines("x") === "2,4,6"){
             message = "You Win!"
-
         } else if(lines("o") === "0,1,2" || lines("o") === "3,4,5" || lines("o") === "6,7,8" || lines("o") === "0,4,8" || lines("o") === "2,4,6")  {
             message = "Computer Wins!"
         } else if (gameBoard.board().every(isNaN)){
             message = "It's a tie!"
-
         } else {
-            if(activePlayer.id === 0) {
-                const userPick = prompt("What's your pick?")
-                gameBoard.pick(userPick, "x")
+            if(currentPlayer.id === 0) {
+                let userPick = NaN
+                while(isNaN(gameBoard.board()[userPick])){
+                    userPick = prompt('Pick your spot')
+                    console.log(`User picks: ${userPick}`)
+
+                }
+                    gameBoard.pick(userPick, "x")
+                    console.log(gameBoard.board())
+                    pick(players()[activePlayer()])
+
+                
+            } else if(currentPlayer.id === 1){
+                let computerPick = NaN
+                while(isNaN(gameBoard.board()[computerPick])){
+                    computerPick = Math.floor(Math.random()*9)
+                    console.log(`Computer picks: ${computerPick}`)
+                }
+                gameBoard.pick(computerPick, "o")
                 console.log(gameBoard.board())
-                pick(players())
-            } else if(activePlayer.id === 1){
-                console.log(cPick())
-                gameBoard.pick(cPick(), "o")
-                console.log(gameBoard.board())
+                pick(players()[activePlayer()])
+
             }   
         }
+
+        
+    
+        
 
         console.log(message)
     }
